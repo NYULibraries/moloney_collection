46222021dd26930c034fe868bd018995bf1e7c30
Gregg Parrish <gregg.alb@gmail.com>
2015-03-09 21:14:06 +0100

    indiv bg img helper, black footer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,9 +9,17 @@ module ApplicationHelper
     end
   end
 
+  def bgimg(page_bg = '')
+    base_bg = image_path("home_header.jpg")
+    if page_bg.empty?
+      base_bg
+    else
+      "#{base_bg}"
+    end
+  end
+
   def nav_link(link_text, link_path)
     class_name = current_page?(link_path) ? 'active' : ''
-
     content_tag(:li, :class => class_name) do
       link_to link_text, link_path
     end
