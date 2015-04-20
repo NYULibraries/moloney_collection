d4742f08499058a366b3b6a857315bd6c7e8f663
Gregg Parrish <gregg.alb@gmail.com>
2015-04-20 14:47:26 +0100

    cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -1,25 +1,23 @@
 Rails.application.routes.draw do
-  resources :finding_aids
-
   mount Ckeditor::Engine => '/ckeditor'
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
   root "static_pages#home"
 
+  resources :artists, except: [:new, :create, :edit, :update, :destroy]
   resources :collections, except: [:new, :create, :edit, :update, :destroy]
   resources :documents, except: [:new, :create, :edit, :update, :destroy]
-  resources :artists, except: [:new, :create, :edit, :update, :destroy]
   resources :finding_aids, except: [:new, :create, :edit, :update, :destroy]
-  resources :songs, except: [:new, :create, :edit, :update, :destroy]
   resources :gaelic_athletes, except: [:new, :create, :edit, :update, :destroy]
   resources :series, except: [:new, :create, :edit, :update, :destroy]
+  resources :songs, except: [:new, :create, :edit, :update, :destroy]
   
   #Static pages
   get "about" => "static_pages#about"
   get "contact" => "static_pages#contact" 
   get "rights-statement" => "static_pages#rights_statement"
-  get "tamiment-library" => "static_pages#tamiment_library"
   get "song-rights" => "static_pages#song_rights"
+  get "tamiment-library" => "static_pages#tamiment_library"
 
 end
