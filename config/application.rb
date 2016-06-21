15fa1232f5e236afc2264b5098b0b171c713dd94
hab278 <hab278@nyu.edu>
2016-06-21 14:27:22 -0400

    Use figs in our application

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,8 @@
 require File.expand_path('../boot', __FILE__)
-
+require 'figs'
 require 'rails/all'
+
+Figs.load(stage: Rails.env)
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
