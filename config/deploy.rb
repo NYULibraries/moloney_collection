79c9851dded8c36241bafcd85c39d17fe3cd9812
BAlter <barnaby.alter@nyu.edu>
2018-11-09 14:31:29 -0500

    Set new relic envs to none

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,7 +4,6 @@ set :app_title, "moloney_collection"
 set :rvm_ruby_string, "2.3.6"
 set :new_relic_environments, ["none"]
 
-set :new_relic_environments, []
 after "deploy:assets:precompile", "deploy:public:copy_system"
 namespace :deploy do
   namespace :public do
