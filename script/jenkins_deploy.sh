1c30b849024e17c9be6f6b8ba7e99255a1cdf301
Eric Griffis <eric.griffis@nyu.edu>
2021-03-15 16:46:03 -0400

    Jenkins deploy - use dockerized curl

--- script/jenkins_deploy.sh
+++ script/jenkins_deploy.sh
@@ -3,7 +3,7 @@
 # get next build number
 export build_number=$( docker run --rm appropriate/curl -f -u $JENKINS_CI_USERNAME:$JENKINS_CI_API_KEY  https://jenkinsci.library.nyu.edu/job/Moloney%20Collection%20Deploy/api/json | jq .nextBuildNumber )
 # trigger build
-curl -f -u $JENKINS_CI_USERNAME:$JENKINS_CI_API_KEY -X POST https://jenkinsci.library.nyu.edu/job/Moloney%20Collection%20Deploy/buildWithParameters\?GIT_COMMIT\=$CIRCLE_SHA1\&DEPLOY_ENV\=$DEPLOY_ENV
+docker run --rm appropriate/curl -f -u $JENKINS_CI_USERNAME:$JENKINS_CI_API_KEY -X POST https://jenkinsci.library.nyu.edu/job/Moloney%20Collection%20Deploy/buildWithParameters\?GIT_COMMIT\=$CIRCLE_SHA1\&DEPLOY_ENV\=$DEPLOY_ENV
 # check build status until success
 check_build_status_until_success() {
   result="no"
